70980b4ec00aa7cf2a01ab6e8f92914f7e9d14fa
Nikhil Singh <135117065+nikhilsingh7777@users.noreply.github.com>
2025-03-16 01:57:21 +0530

    Update utils.js

--- src/components/utils/utils.js
+++ src/components/utils/utils.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const newRequest = axios.create({
-    baseURL: process.env.REACT_PUBLIC_API_URL || 'https://cognitic-backend-8.onrender.com',
+    baseURL:'https://cognitic-backend-8.onrender.com',
     withCredentials: true,
 });
 export default newRequest;
