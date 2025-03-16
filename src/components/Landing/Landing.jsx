e4c1f5bfdfac75b688d5dd669fc82244e2699be0
Nikhil Singh <135117065+nikhilsingh7777@users.noreply.github.com>
2025-03-16 18:58:01 +0530

    Update Landing.jsx

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -14,13 +14,13 @@ const Landing = () => {
 
     return (
         <>
-            <p>Please select a service</p>
+            <p>COGNIZANCE'25 ACCO. & SCAN PORTAL</p>
             <nav>
                 <ul>
-                    <li><Link to="/ticketing">Ticketing</Link></li>
+                    <li><Link to="/ticketing">SCAN Portal</Link></li>
+                     <li><Link to="/controls">Controls</Link></li>
                     <li><Link to="/alloter">Alloter</Link></li>
-                    <li><Link to="/controls">Controls</Link></li>
-                    <li><Link to="/excel-upload">Bulk.Mailer</Link></li> {/* Added */}
+                    <li><Link to="/excel-upload">Mailer</Link></li> {/* Added */}
                 </ul>
             </nav>
         </>
